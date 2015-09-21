4814b406cbba4c19b0957b5a663c1ad4580953bc
Loren Barrick <lorenbarrick@gmail.com>
2015-09-21 13:43:01 -0700

    add redis

--- app.js
+++ app.js
@@ -4,17 +4,31 @@ var bodyParser = require('body-parser');
 var urlencode = bodyParser.urlencoded({ extended: false });
 
 app.use(express.static('public'));
+var redis = require('redis');
+var client = redis.createClient();
+
+client.select(process.env.NODE_ENV.length || 'development'.length);
+
+
 app.get('/cities', function(request, response) {
   var cities = {
     'Lotopia': 'some description',
     'Caspiana': 'description',
     'Indigo': 'description'
   };
-  response.json(Object.keys(cities));
+  client.hkeys('cities', function(error, names){
+    if(error) throw error;
+      response.json(names);
+  });
+
 });
 app.post('/cities', urlencode, function(request, response) {
   var newCity = request.body;
+  client.hset('cities', newCity.name, newCity.description, function(error) {
+      if(error) throw error;
+      response.status(201).json(newCity.name);
+  });
   cities[newCity.name] = newCity.description;
-  response.status(201).json(newCity.name);
+
 });
 module.exports = app;
